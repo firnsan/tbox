bb97a616b4b986569d86437f623aae06236c7b29
firnsan <firnsan@gmail.com>
2015-05-15 03:55:06 +0800

    添加condition

---
+++ tbox/Condition.h
@@ -0,0 +1,49 @@
+#ifndef TBOX_CONDITION_H
+#define TBOX_CONDITION_H
+
+#include <pthread.h>
+
+
+#include "Mutex.h"
+#include "NonCopyable.h"
+
+
+namespace tbox
+{
+	class Condition : NonCopyable
+	{
+	public:
+		explicit Condition(MutexLock &mutex)
+			: mutex_(mutex)
+		{
+			pthread_cond_init(&cond_, NULL);
+		}
+
+		~Condition()
+		{
+			pthread_cond_destroy(&cond_);
+		}
+
+		void wait()
+		{
+			pthread_cond_wait(&cond_, mutex_.get());
+		}
+
+		void notify()
+		{
+			pthread_cond_signal(&cond_);
+		}
+
+		void notifyAll()
+		{
+			pthread_cond_broadcast(&cond_);
+		}
+
+	private:
+		// const MutexLock &mutex_; 不可以const，否则得提供const版的MutexLock::get()
+		MutexLock &mutex_;
+		pthread_cond_t cond_;
+	};
+}
+
+#endif
